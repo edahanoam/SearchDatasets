9b744930526484f9853f5143b5141cf50635bf68
Noam Dahan <noam.dahan1@cs.huji.ac.il>
2024-09-02 17:08:23 +0300

    works only on local file

--- main.py
+++ main.py
@@ -7,15 +7,24 @@ def page_set():
     st.title("Summarization Datasets Search Engine")
 
 
-def connect_to_doc():
-    # Connect to the Google Sheet
-    url = "https://docs.google.com/spreadsheets/d/1uJqKosaAD0paPTJHT7-g0Ssw_P-X1Mp7aOchFFKGN4M/edit?usp=sharing"
-    df = pd.read_csv(url, dtype=str).fillna("")
-    st.write(df)
+def connect_to_doc(local=False):
+    if not local:
+        # Connect to the Google Sheet
+        url = "https://docs.google.com/spreadsheets/d/1uJqKosaAD0paPTJHT7-g0Ssw_P-X1Mp7aOchFFKGN4M/edit?usp=sharing"
+        df = pd.read_csv(url, dtype=str).fillna("")
+        st.write(df)
+    else:
+        df = pd.read_csv("after_unifying.csv", dtype=str).fillna("")
+        st.write(df)
+
+
+
+
 
 if __name__ == '__main__':
     page_set()
-    connect_to_doc()
+    connect_to_doc(True)
+
 
 
 
