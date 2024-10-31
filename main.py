0ac6111fa5f8911f9ba85dda766874bd1325e030
edahanoam1 <90025303+edahanoam1@users.noreply.github.com>
2024-10-31 14:33:21 +0200

    remove unavailiable datasets

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def connect_to_doc(local=False):
             df = df.drop(columns=['Unnamed: 0'],axis=1)
         df.rename(columns={'Where':'Venue',"Published Year":"Date",'Paper Name ':'Paper'},
                   inplace=True)
-        df = df[df['Availability'] != 'Unknown']
+        #df = df[df['Availability'] != 'Unknown']
 
         return df
 
