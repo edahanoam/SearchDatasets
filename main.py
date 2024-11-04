7f583045442f45d8791b07f59946394c7d7886e3
edahanoam <46026111+edahanoam@users.noreply.github.com>
2024-11-04 17:57:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def load_datasets():
 
 def page_set():
     st.set_page_config(page_title="Dataset Search", layout="wide")
-    st.title("Explore Summarization Datasets")
+    st.title("Summarization Dataset Search Platform")
     if 'show_form' not in st.session_state:
         st.session_state.show_form = False
 
